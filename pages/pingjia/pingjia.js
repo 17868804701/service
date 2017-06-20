c1e64759bf76d23400b1ca767ea28d72aeacf09c
17868804701 <742210203@qq.com>
2017-06-20 23:39:52 +0800

    添加评价页面

--- pages/pingjia/pingjia.js
+++ pages/pingjia/pingjia.js
@@ -5,6 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    files: [],
+    star1:'★',
+    star2: '★',
+    star3: '★',
+    star4: '★',
+    star5: '★',
   
   },
 
@@ -62,5 +68,96 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+      }
+    })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+  // 相机评价
+  click_1: function () {
+    console.log("1星评价")
+    if (this.data.star1 == "★") {
+      this.setData({
+        star5: '☆',
+        star4: '☆',
+        star3: '☆',
+        star2: '☆',
+      })
+    } else {
+      this.setData({
+        star1: '★',
+      })
+    }
+  },
+  click_2: function () {
+    console.log("2星评价")
+    if (this.data.star2 == '★') {
+      this.setData({
+        star5: '☆',
+        star4: '☆',
+        star3: '☆',
+
+      })
+    } else {
+      this.setData({
+        star1: '★',
+        star2: '★',
+      })
+    }
+  },
+  click_3: function () {
+    console.log("3星评价")
+    if (this.data.star3 == "★") {
+      this.setData({
+        star5: '☆',
+        star4: '☆',
+      })
+    } else {
+      this.setData({
+        star1: '★',
+        star2: '★',
+        star3: '★',
+      })
+    }
+  },
+  click_4: function () {
+    console.log("4星评价")
+    if (this.data.star4 == '★') {
+      this.setData({
+        star5: '☆',
+      })
+    } else {
+      this.setData({
+        star1: '★',
+        star2: '★',
+        star3: '★',
+        star4: '★',
+      })
+    }
+  },
+  click_5: function () {
+    console.log("5星评价");
+    this.setData({
+      star1: '★',
+      star2: '★',
+      star3: '★',
+      star4: '★',
+      star5: '★',
+    })
   }
 })
